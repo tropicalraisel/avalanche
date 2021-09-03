2ca69df0f24c40f606f3258dbbf8810f3310a1f0
TropicalRaisel <tshuler@nationalraisin.com>
2021-09-03 08:31:12 -0700

    Snowpack baseline

---
+++ snowpack.config.mjs
@@ -0,0 +1,21 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
